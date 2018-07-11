2548a8df6da12d7b3c50ecd07042a3e0cbbc158f
郑延 <jesonxiake@126.com>
2018-07-11 08:10:29 +0800

    Read record time order

--- newsletter/news/models.py
+++ newsletter/news/models.py
@@ -48,6 +48,8 @@ class ReadRecord(models.Model):
     news = models.ForeignKey(News, on_delete=models.CASCADE, related_name="read_records")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="read_records")
     time = models.DateTimeField(null=True)
+    class Meta:
+        ordering = ['-time']
 
 from django.dispatch import receiver
 from django.db.models.signals import post_save
